2457ef779d3b3f9281c95622e71bb6f02f8694fb
Madison Schott <madison@Madisons-MacBook-Pro.local>
2024-05-10 15:11:22 -0700

    dbt project

---
+++ macros/split_column_values.sql
@@ -0,0 +1,4 @@
+{% macro split_column_values(column_name, new_column_name, new_column_name_2) -%} 
+    -- create custom macro to trim priority just to code and put status in another field 
+    
+{%- endmacro %}
